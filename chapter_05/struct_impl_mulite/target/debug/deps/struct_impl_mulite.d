4f5577af200d1be97fe7179c24325fbbd4fc0119
Liumanman <631925002@qq.com>
2022-03-28 17:27:17 +0800

    struct methods update

---
+++ chapter_05/struct_impl_mulite/target/debug/deps/struct_impl_mulite.d
@@ -0,0 +1,5 @@
+C:\code\rustCode\learning-rust\chapter_05\struct_impl_mulite\target\debug\deps\struct_impl_mulite.exe: src\main.rs
+
+C:\code\rustCode\learning-rust\chapter_05\struct_impl_mulite\target\debug\deps\struct_impl_mulite.d: src\main.rs
+
+src\main.rs:
---
+++ chapter_05/struct_impl_mulite/target/debug/struct_impl_mulite.d
@@ -0,0 +1,1 @@
+C:\code\rustCode\learning-rust\chapter_05\struct_impl_mulite\target\debug\struct_impl_mulite.exe: C:\code\rustCode\learning-rust\chapter_05\struct_impl_mulite\src\main.rs
